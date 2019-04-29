161cba0ddd795e0bb50f9df3e06cef47b042d61b
Danletski <siletski.dan@gmail.com>
2019-04-29 21:04:45 +0300

    Update sitePrinter.py

--- sitePrinter.py
+++ sitePrinter.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 
 #Driver location
-driver = webdriver.Chrome(executable_path='./chromedriver')
+driver = webdriver.Chrome(executable_path='/Users/dans/Documents/GitHub/Project2/chromedriver')
 
 #Open website
 driver.get('http://192.168.99.100:5000')
