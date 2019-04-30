15f789e414b0d50ff55047f19e3b0dff6069cf1b
Danletski <siletski.dan@gmail.com>
2019-04-30 12:39:36 +0300

    Update sitePrinter.py

--- sitePrinter.py
+++ sitePrinter.py
@@ -2,9 +2,9 @@
 from selenium import webdriver
 
 #Driver location
-driver = webdriver.Chrome(executable_path='/Users/dans/Documents/GitHub/Project2/chromedriver')
+driver = webdriver.Chrome(executable_path='/Users/dans/Documents/Devops/chromedriver')
 
-#Open website
+#Open website (Docker IP).
 driver.get('http://192.168.99.100:5000')
 
 
@@ -12,8 +12,10 @@ driver.get('http://192.168.99.100:5000')
 driver.implicitly_wait(3)
 
 #Locate text
-#html = driver.page_source
-print('123')
+html = driver.find_element_by_xpath('//html/body')
+    #driver.find_elements_by_css_selector('textarea')[0]
+
+print(html.text)
 
 #Close page
 driver.close()
